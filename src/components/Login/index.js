4ea3d33d0a5c88a301680e057cece80648a0da19
Vishnu Gopy <vishnugoby0010@gmail.com>
2021-07-27 22:32:10 +0200

    adding fotms

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
 import userServices from "../../services/userService";
